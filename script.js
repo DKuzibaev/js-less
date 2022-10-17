bdd73e893427bed19e7afd988bf2ee88d4e29a69
Doniyor <doniyor.kuzibaev@yandex.ru>
2022-10-17 18:21:45 +0500

    add new desc add js lesson

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 "use strict";
 
-console.log("Hello World");
+let numbresOfFilms = +prompt("Сколько фильмов уже смотрели?", ' ');
+
+const personalMovieDB = {
+    count: numbresOfFilms,
+    movies: {},
+    actors: {},
+    genders: [],
+    privrite: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", " "),
+      b = prompt("Насколько оцените его?", " "),
+      c = prompt("Один из последних просмотренных фильмов?", " "),
+      d = prompt("Насколько оцените его?", " ");
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
