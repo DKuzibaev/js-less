f0deaa6311061fa6247f8f12917a425f5bfcd0ff
DoniyorK <doniyorkuzibaev@gmail.com>
2022-10-31 20:03:20 +0500

    add files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/*let numbresOfFilms = +prompt("Сколько фильмов уже смотрели?", ' ');
+let numbresOfFilms = +prompt("Сколько фильмов уже смотрели?", ' ');
 
 const personalMovieDB = {
     count: numbresOfFilms,
@@ -10,23 +10,41 @@ const personalMovieDB = {
     privrite: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", " "),
-      b = prompt("Насколько оцените его?", " "),
-      c = prompt("Один из последних просмотренных фильмов?", " "),
-      d = prompt("Насколько оцените его?", " ");
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i ++) {
+    const a = prompt("Один из последних просмотренных фильмов?", " "),
+          b = prompt("Насколько оцените его?", " ");
+    personalMovieDB.movies[a] = b;
+    
+}      
+
 
 console.log(personalMovieDB);
-*/
 
 
-const arr = [3, 5, 8, 16, 20, 23, 50];
-const result = [];
 
-for(let i = 3; i <= 50; i++) {
-    if (i % 3 == 0)
-    result[i - 50] = i++;
-    console.log(result);
-}
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for(let i = 3; i <= 50; i++) {
+//     if (i % 3 == 0)
+//     result[i - 50] = i++;
+//     console.log(result);
+// }
+// let userName = prompt();
+// let admin = userName;
+// alert(admin);
+
+// alert( `hello ${1}`); 
+// alert( `hello ${"name"}`);
+// alert( `hello ${admin}`); 
+
+// let number = 10;
+
+// for(let i = 0; i < number; i++) {
+//     if (i === 5 ) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
